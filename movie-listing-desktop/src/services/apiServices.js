426bbeb31bf2cb359a754720e868501684fa3016
Infant Ajay Venus <infantcsajay@outlook.com>
2020-06-06 22:45:13 +0530

    ...

--- movie-listing-desktop/src/services/apiServices.js
+++
@@ -1,9 +0,0 @@
-import {MDB_KEY} from '../../env.json';
-
-export const fetchListProperty = propertyName =>
-    async pageNumber =>
-        await fetch(`https://api.themoviedb.org/3/movie/${propertyName}?api_key=${process.env.MDB_KEY}&language=en-US&page=${pageNumber}`);
-
-export const searchByName = name =>
-    async pageNumber =>
-        await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MDB_KEY}&query=${encodeURIComponent(name)}&page=${pageNumber}`);
