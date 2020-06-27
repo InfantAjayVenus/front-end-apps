bbb440bd98ac4d1d28060a521d93f15eb854d07b
InfantAjayVenus <infantvenusajay@gmail.com>
2020-06-27 12:00:05 +0530

    added index for presentations

--- presentations/behavioural-patterns/src/scripts/index.js
+++
@@ -1,6 +0,0 @@
-import './webslides';
-import highlight from 'highlight.js/lib/core';
-import java from 'highlight.js/lib/languages/java';
-
-highlight.initHighlightingOnLoad();
-highlight.registerLanguage('java', java);
--- presentations/design-patterns-intro/src/scripts/index.js
+++
@@ -1,6 +0,0 @@
-import './webslides';
-import highlight from 'highlight.js/lib/core';
-import typescript from 'highlight.js/lib/languages/typescript';
-
-highlight.initHighlightingOnLoad();
-highlight.registerLanguage('typescript', typescript);
